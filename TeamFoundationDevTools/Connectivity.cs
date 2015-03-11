a59ea439355a7bf9a6d6994c431300c3cc015691
Aarsh Talati <AarshSTalati1989@gmail.com>
2015-03-11 17:31:24 -0400

    swithced assemblies and publish preferences

--- TeamFoundationDevTools/Connectivity.cs
+++ TeamFoundationDevTools/Connectivity.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
 
--- TeamFoundationDevTools/Search.cs
+++ TeamFoundationDevTools/Search.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
 
