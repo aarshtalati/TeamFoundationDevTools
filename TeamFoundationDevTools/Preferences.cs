fb1da079b478be08984580449664978f0803f2bc
unknown <UGAT174@DEV-ATALATI-DT1.Development.com>
2015-03-12 12:16:50 -0400

    compiled source in Windows XP .NET 3.5 using Vs2k8

--- TeamFoundationDevTools/Preferences.cs
+++ TeamFoundationDevTools/Preferences.cs
@@ -5,76 +5,80 @@ using System.Text;
 
 namespace TeamFoundationDevTools
 {
-	enum PreferenceMenu
-	{
-		View,
-		Change
-	}
+    enum PreferenceMenu
+    {
+        View,
+        Change
+    }
 
-	static class Preferences
-	{
-		public static string outputFilePath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
-		
-		internal static string GetFileName()
-		{
-			return string.Format("TFSSearch_{0}.txt", DateTime.Now.ToString("__yyyy_MM_dd_HH_mm_ss_fff"));
-		}
+    static class Preferences
+    {
+        public static string outputFilePath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
 
-		public static bool detailedScreenOutput = false;
-		public static bool detailedFileOutput = true;
+        internal static string GetFileName()
+        {
+            return string.Format("TFSSearch_{0}.txt", DateTime.Now.ToString("__yyyy_MM_dd_HH_mm_ss_fff"));
+        }
 
-		
+        public static bool detailedScreenOutput = false;
+        public static bool detailedFileOutput = true;
 
-		internal static void PreferencesHome(PreferenceMenu? choice = null)
-		{
-			switch (choice)
-			{
-				case PreferenceMenu.View:
-					ViewPreferences(true);
-					break;
+        // Syntactic sugar for optional parameter ( if yer usin' Vs 2k8 ! )
+        internal static void PreferencesHome() { PreferencesHome(null); }
 
-
-				case PreferenceMenu.Change:
-					ChangePreferences();
-					break;
+        internal static void PreferencesHome(PreferenceMenu? choice)
+        {
+            switch (choice)
+            {
+                case PreferenceMenu.View:
+                    ViewPreferences(true);
+                    break;
 
 
-				default:
-					Console.WriteLine("Preferences Operations :");
-					Console.WriteLine("0.\t View");
-					Console.WriteLine("1.\t Change");
-					string message = "Your Selection : ";
-					int selection = Utils.GetValidIntChoice(-1, 0, 1, ref message);
-
-					if(selection == (int)PreferenceMenu.View)
-							ViewPreferences();
-					else
-							ChangePreferences();
-
-					break;
-			}
+                case PreferenceMenu.Change:
+                    ChangePreferences();
+                    break;
 
 
-		}
+                default:
+                    Console.WriteLine("Preferences Operations :");
+                    Console.WriteLine("0.\t View");
+                    Console.WriteLine("1.\t Change");
+                    string message = "Your Selection : ";
+                    int selection = Utils.GetValidIntChoice(-1, 0, 1, ref message);
 
-		static void ViewPreferences(bool displayOnly = false)
-		{
-			Console.WriteLine(("Search Preferences for :").PadRight(50, ' ') + " " + Connectivity.tfsProjectCollectionUri.ToString());
-			Console.WriteLine(("Results To :").PadRight(50, ' ') + " " + outputFilePath + "\\" + GetFileName() + " (file name may vary)");
-			Console.WriteLine(("Detailed Output on Screen :").PadRight(50, ' ') + " {0}", detailedScreenOutput ? "Yes" : "No");
-			Console.WriteLine(("Detailed Output in txt :").PadRight(50, ' ') + " {0}", detailedFileOutput ? "Yes" : "No");
+                    if (selection == (int)PreferenceMenu.View)
+                        ViewPreferences();
+                    else
+                        ChangePreferences();
 
-			if (!displayOnly)
-			{
-				Console.WriteLine("\n\n\n>>> Press Any key to return.");
-				Console.ReadKey();
-			}
-		}
+                    break;
+            }
 
-		static void ChangePreferences()
-		{
-			Console.WriteLine("YET TO BE IMPLEMENTED ... ");
-			Console.ReadKey();
-		}
-	}
+
+        }
+
+        // Syntactic sugar for optional parameter ( if yer usin' Vs 2k8 ! )
+        static void ViewPreferences() { ViewPreferences(false); }
+
+        static void ViewPreferences(bool displayOnly)
+        {
+            Console.WriteLine(("Search Preferences for :").PadRight(50, ' ') + " " + Connectivity.tfsProjectCollectionUri.ToString());
+            Console.WriteLine(("Results To :").PadRight(50, ' ') + " " + outputFilePath + "\\" + GetFileName() + " (file name may vary)");
+            Console.WriteLine(("Detailed Output on Screen :").PadRight(50, ' ') + " {0}", detailedScreenOutput ? "Yes" : "No");
+            Console.WriteLine(("Detailed Output in txt :").PadRight(50, ' ') + " {0}", detailedFileOutput ? "Yes" : "No");
+
+            if (!displayOnly)
+            {
+                Console.WriteLine("\n\n\n>>> Press Any key to return.");
+                Console.ReadKey();
+            }
+        }
+
+        static void ChangePreferences()
+        {
+            Console.WriteLine("YET TO BE IMPLEMENTED ... ");
+            Console.ReadKey();
+        }
+    }
 }
--- TeamFoundationDevTools/Search.cs
+++ TeamFoundationDevTools/Search.cs
@@ -8,160 +8,172 @@ using Microsoft.TeamFoundation.VersionControl.Client;
 
 namespace TeamFoundationDevTools
 {
-	static class Search
-	{
+    static class Search
+    {
 
-		static int results = 0;
-		static CultureInfo locale = CultureInfo.CurrentCulture;
+        static int results = 0;
+        static CultureInfo locale = CultureInfo.CurrentCulture;
 
-		static string fileName = null;
-		internal static string GetFileName()
-		{
-			return Preferences.outputFilePath + "\\" + Preferences.GetFileName();
-		}
+        static string fileName = null;
+        internal static string GetFileName()
+        {
+            return Preferences.outputFilePath + "\\" + Preferences.GetFileName();
+        }
 
-		static string
-			fileNameWildCard = null,
-			filePathContains = null;
+        static string
+            fileNameWildCard = null,
+            filePathContains = null;
 
-		internal static void FullSearch(Uri TpcAddress = null)
-		{
-			Console.WriteLine("Search Parameters :");
+        // Syntactic sugar for optional parameter ( if yer usin' Vs 2k8 ! )
+        internal static void FullSearch() { FullSearch(null); }
 
-			Preferences.PreferencesHome(PreferenceMenu.View);
+        internal static void FullSearch(Uri TpcAddress)
+        {
+            Console.WriteLine("Search Parameters :");
 
-			Console.Write(("File Name ( Wild Card ) :").PadRight(51));
-			fileNameWildCard = Console.ReadLine();
+            Preferences.PreferencesHome(PreferenceMenu.View);
 
-			if (!fileNameWildCard.Contains('*') || fileNameWildCard.Contains('?'))
-				fileNameWildCard = "*" + fileNameWildCard + "*";
+            Console.Write(("File Name ( Wild Card ) :").PadRight(51));
+            fileNameWildCard = Console.ReadLine();
 
-			Console.Write(("File Path Contains ( Ignore-Case ) :").PadRight(51));
-			filePathContains = Console.ReadLine();
+            if (!fileNameWildCard.Contains('*') || fileNameWildCard.Contains('?'))
+                fileNameWildCard = "*" + fileNameWildCard + "*";
 
-			if (TpcAddress == null || fileNameWildCard == null || fileNameWildCard.Length == 0)
-				return;
+            Console.Write(("File Path Contains ( Ignore-Case ) :").PadRight(51));
+            filePathContains = Console.ReadLine();
 
-			var tpc = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(TpcAddress);
-			tpc.Authenticate();
-			tpc.EnsureAuthenticated();
+            if (TpcAddress == null || fileNameWildCard == null || fileNameWildCard.Length == 0)
+                return;
 
-			var versionControl = tpc.GetService<VersionControlServer>();
-			var projects = versionControl.GetAllTeamProjects(true);
+            var tpc = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(TpcAddress);
+            tpc.Authenticate();
+            tpc.EnsureAuthenticated();
 
-			StringBuilder sbContent = new StringBuilder();
-			sbContent.AppendLine("===================");
-			sbContent.AppendLine("*** Search Criteria : ***");
-			sbContent.AppendLine("===================");
+            var versionControl = tpc.GetService<VersionControlServer>();
+            var projects = versionControl.GetAllTeamProjects(true);
 
-
-			sbContent.AppendLine();
-			sbContent.AppendLine("Server :" + TpcAddress.OriginalString);
-			sbContent.AppendLine("Pattern :" + fileNameWildCard);
-
-			if (filePathContains != null || filePathContains.Length != 0)
-				sbContent.AppendLine("File Path Filter : " + filePathContains);
-
-			sbContent.AppendLine("\n\n\n\n");
-
-			sbContent.AppendLine("===================");
-			sbContent.AppendLine("*** Results : ***");
-			sbContent.AppendLine("===================");
-
-			sbContent.AppendLine(Environment.NewLine + Environment.NewLine);
-
-			string fileName = GetFileName();
-
-			string project_progress;
-			foreach (var project in projects)
-			{
-				project_progress = null;
-				var items = versionControl.GetItems(project.ServerItem + "/" + fileNameWildCard, RecursionType.Full).Items;
-
-				if (items.Length > 0)
-				{
-
-					if (filePathContains != null && filePathContains.Length != 0)
-					{
-						//items = items.Where(p => p.ServerItem.Contains(filePathContains)).ToArray();
-						items = items.Where(p => locale.CompareInfo.IndexOf(p.ServerItem, filePathContains, CompareOptions.IgnoreCase) > 0).ToArray();
-					}
-
-					if (items.Length == 0)
-					{
-						Console.ForegroundColor = ConsoleColor.DarkCyan;
-						project_progress = string.Format(">>\t {0} \t {1}", ("path filter in :").PadLeft(25, '.'), project.Name);
-						sbContent.AppendLine();
-						sbContent.AppendLine(project_progress);
-						Console.WriteLine(project_progress);
-						Console.ResetColor();
-						continue;
-					}
-
-					Console.ForegroundColor = ConsoleColor.Green;
-					project_progress = string.Format("{0}\t {1} \t {2}", items.Length, ("MATCH FOUND in :").PadLeft(25, '.'), project.Name);
-					sbContent.AppendLine();
-					sbContent.AppendLine(project_progress);
-					Console.WriteLine(project_progress);
-					Console.ResetColor();
-
-					results += items.Length;
-
-					sbContent.AppendLine();
-
-					foreach (var item in items)
-					{
-						var history = (IEnumerable<Changeset>)(versionControl.QueryHistory(item.ServerItem, VersionSpec.Latest, 0, RecursionType.OneLevel, null, null, null, int.MaxValue, true, false, true));
-						var h = history.FirstOrDefault();
-
-						string
-							changesetId = null,
-							commiter = null,
-							committedOn = null,
-							checkinNote = null;
-
-						if (h == null)
-							changesetId = commiter = committedOn = checkinNote = "( ? )";
-
-						else
-						{
-							changesetId = h.ChangesetId.ToString();
-							commiter = (h.CommitterDisplayName ?? "").ToUpper();
-							committedOn = (h.CreationDate.ToString("MM/dd/yy HH:mm:ss tt") ?? "").ToUpper();
-
-							if (h.CheckinNote.Values.Count() > 0)
-							{
-								checkinNote = (h.CheckinNote.Values[0].Value ?? "").ToUpper();
-							}
-						}
+            StringBuilder sbContent = new StringBuilder();
+            sbContent.AppendLine("===================");
+            sbContent.AppendLine("*** Search Criteria : ***");
+            sbContent.AppendLine("===================");
 
 
-						sbContent.AppendFormat("\t\t\t {0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}" + Environment.NewLine, changesetId, commiter, committedOn, checkinNote, item.ServerItem);
-					}
-					sbContent.AppendLine();
+            sbContent.AppendLine();
+            sbContent.AppendLine("Server :" + TpcAddress.OriginalString);
+            sbContent.AppendLine("Pattern :" + fileNameWildCard);
 
-					Utils.DumpData(ref sbContent, ref fileName);
+            if (filePathContains != null || filePathContains.Length != 0)
+                sbContent.AppendLine("File Path Filter : " + filePathContains);
 
-				}
-				else
-				{
-					project_progress = string.Format("-\t {0} \t {1}", ("------- in :").PadLeft(25, '.'), project.Name);
-					sbContent.AppendLine();
-					sbContent.AppendLine(project_progress);
-					Console.WriteLine(project_progress);
+            sbContent.AppendLine("\n\n\n\n");
 
-					Utils.DumpData(ref sbContent, ref fileName);
-				}
-			}
+            sbContent.AppendLine("===================");
+            sbContent.AppendLine("*** Results : ***");
+            sbContent.AppendLine("===================");
+
+            sbContent.AppendLine(Environment.NewLine + Environment.NewLine);
+
+            string fileName = GetFileName();
+
+            string project_progress;
+            foreach (var project in projects)
+            {
+                project_progress = null;
+                var items = versionControl.GetItems(project.ServerItem + "/" + fileNameWildCard, RecursionType.Full).Items;
+
+                if (items.Length > 0)
+                {
+
+                    if (filePathContains != null && filePathContains.Length != 0)
+                    {
+                        //items = items.Where(p => p.ServerItem.Contains(filePathContains)).ToArray();
+                        items = items.Where(p => locale.CompareInfo.IndexOf(p.ServerItem, filePathContains, CompareOptions.IgnoreCase) > 0).ToArray();
+                    }
+
+                    if (items.Length == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        project_progress = string.Format(">>\t {0} \t {1}", ("path filter in :").PadRight(25, '.'), project.Name);
+                        sbContent.AppendLine();
+                        sbContent.AppendLine(project_progress);
+                        Console.WriteLine(project_progress);
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    project_progress = string.Format("{0}\t {1} \t {2}", items.Length, ("MATCH FOUND in :").PadRight(25, '.'), project.Name);
+                    sbContent.AppendLine();
+                    sbContent.AppendLine(project_progress);
+                    Console.WriteLine(project_progress);
+                    Console.ResetColor();
+
+                    results += items.Length;
+
+                    sbContent.AppendLine();
+
+                    foreach (var item in items)
+                    {
+                        var history = versionControl.QueryHistory(item.ServerItem, VersionSpec.Latest, 0, RecursionType.OneLevel, null, null, null, 1, true, true);
+                        Changeset h = null;
+                        
+                        var enumer = history.GetEnumerator();
+                        int count = 0;
+
+                        while (count < 1 && enumer.MoveNext())
+                        {
+                            h = (Changeset)enumer.Current;
+                            count++;
+                        }
+
+                        string
+                            changesetId = null,
+                            commiter = null,
+                            committedOn = null,
+                            comment = null;
+
+                        if (h == null)
+                            changesetId = commiter = committedOn = comment = "( ? )";
+
+                        else
+                        {
+                            changesetId = h.ChangesetId.ToString();
+                            commiter = (h.Committer ?? "").ToUpper();
+                            committedOn = (h.CreationDate.ToString("MM/dd/yy HH:mm:ss tt") ?? "").ToUpper();
+                            comment = h.Comment ?? "";
+                        }
+
+                        sbContent.AppendFormat("\t{0} {1} {2} {3} {4}" +Environment.NewLine, 
+                            changesetId.PadRight(10), 
+                            commiter.PadRight(60), 
+                            committedOn.PadRight(25),
+                            comment.Length > 100 ? comment.Substring(0, 100).PadRight(105) : comment.PadRight(105), 
+                            item.ServerItem);
+                    }
+                    sbContent.AppendLine();
+
+                    Utils.DumpData(ref sbContent, ref fileName);
+
+                }
+                else
+                {
+                    project_progress = string.Format("-\t {0} \t {1}", ("------- in :").PadRight(25, '.'), project.Name);
+                    sbContent.AppendLine();
+                    sbContent.AppendLine(project_progress);
+                    Console.WriteLine(project_progress);
+
+                    Utils.DumpData(ref sbContent, ref fileName);
+                }
+            }
 
 
-			sbContent.AppendLine(string.Format("{0} Found", results));
-			Utils.DumpData(ref sbContent, ref fileName);
+            sbContent.AppendLine(string.Format("{0} Found", results));
+            Utils.DumpData(ref sbContent, ref fileName);
 
-			Console.WriteLine("\n\n\nDone. {0} Found.", results);
-			Console.WriteLine("Output File : " + fileName);
-			Console.ReadKey();
-		}
+            Console.WriteLine("\n\n\nDone. {0} Found.", results);
+            Console.WriteLine("Output File : " + fileName);
+            Console.ReadKey();
+        }
 
-	}
+    }
 }
